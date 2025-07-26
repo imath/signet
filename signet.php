caadb5375d7bcb971c3900222b9b9ea4447f0822
imath <math.viet@gmail.com>
2025-07-26 02:32:36 +0200

    Add plugin main file & the `.gitignore` one

---
+++ signet.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Format bookmarks using a rich representation of the corresponding linked Web pages.
+ *
+ * @package   Signet
+ * @author    imath
+ * @license   GPL-2.0+
+ * @link      https://imathi.eu
+ *
+ * @retraceur-block
+ * Plugin Name:        Signet
+ * Plugin URI:         https://github.com/imath/signet
+ * Plugin Type:        block
+ * Description:        Format bookmarks using a rich representation of the corresponding linked Web pages.
+ * Version:            1.0.0
+ * Author:             imath
+ * Author URI:         https://imathi.eu
+ * Requires Retraceur: 1.0.0
+ * Up to Retraceur:    2.0.0-alpha
+ * Requires PHP:       5.6
+ * Text Domain:        signet
+ * License:            MIT License
+ * License URI:        https://github.com/imath/signet/blob/trunk/LICENSE.md
+ * Domain Path:        /languages/
+ * GitHub Plugin URI:  https://github.com/imath/signet
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Register the Signet Block.
+ *
+ * @since 1.0.0
+ */
+function signet_block_init() {
+	register_block_type( dirname( __FILE__ ) . '/build' );
+}
+add_action( 'init', 'signet_block_init' );
