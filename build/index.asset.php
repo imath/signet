7705094ffa994728b7a2cde7fa705cd015d8f7c6
imath <imathi.eu@outlook.fr>
2025-07-28 22:14:33 +0200

    Include the Media Text block styles
    Let's use it to organize the Signet Block display.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '88750f03d3de0e071bd8');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2d2e7dbf6e279f0bc68a');
